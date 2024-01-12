a200d842ab166f40054d8bdda556134a629ca982
Lawrence Chillrud <lgc2139@columbia.edu>
2024-01-12 16:41:49 -0600

    Updated dcm to nifti conversion

--- code/preprocessing/2a_convert_dicoms_to_nifti.py
+++ code/preprocessing/2a_convert_dicoms_to_nifti.py
@@ -1,4 +1,4 @@
-# File: 2_convert_dicoms_to_nifti.py
+# File: 2a_convert_dicoms_to_nifti.py
 # Date: 01/08/2024
 # Author: Lawrence Chillrud <chili@u.northwestern.edu>
 # Description: This script converts dicom files to nifti files.
@@ -14,19 +14,23 @@
 #--------------------------#
 ####      N. NOTES      ####
 #--------------------------#
-# This script is meant to
+# This script converts dicom files to nifti files, 
+# taking roughly 15-20min to convert all Brainlab scans as of 1/12/24.
 #
 # This script relies on the following file(s) as inputs:
 #   * data/preprocessing/NURIPS_downloads/Meningiomas_handchecked/*/*_Brainlab/ready_for_preprocessing/*/resources/DICOM/*.dcm
+#   * code/preprocessing/dcm2niix
 #
 # This script generates the following file(s) as outputs:
 #   * data/preprocessing/output/2_NIFTI/*/*_Brainlab/*.nii.gz
+#   * data/preprocessing/output/2_NIFTI/log.txt
 
 #--------------------------#
 #### 0. PACKAGE IMPORTS ####
 #--------------------------#
 import os
 from tqdm import tqdm
+from datetime import datetime
 
 def lsdir(path):
     return sorted([d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))])
@@ -42,12 +46,17 @@ data_dir = 'data/preprocessing/NURIPS_downloads/Meningiomas_handchecked'
 dcm = 'resources/DICOM'
 output_dir = 'data/preprocessing/output/2_NIFTI'
 log_file = 'data/preprocessing/output/2_NIFTI/log.txt'
+dcm2niix = './code/preprocessing/dcm2niix'
 
 if not os.path.exists(output_dir): os.makedirs(output_dir)
+
 #---------------------------------#
 #### 2. CONVERT DICOMS 2 NIFTI ####
 #---------------------------------#
-os.system(f"echo '' > {log_file}")
+date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+bar = '-' * 80
+os.system(f"echo '\n{bar}\n' >> {log_file}")
+os.system(f"echo 'Running script 2a_convert_dicoms_to_nifti.py at {date}\n' >> {log_file}")
 print(f"Logging output to {log_file}")
 
 # wrap this for loop with a progress bar using tqdm
@@ -59,7 +68,7 @@ for subject in tqdm(lsdir(data_dir), desc="Subjects"):
             if not os.path.exists(cur_output_dir): 
                 os.makedirs(cur_output_dir)
                 log_cmd = f"echo '\nConverting {session}/{scan}' >> {log_file}"
-                convert_cmd = f"dcm2niix -z y -9 -d -v 0 -o {cur_output_dir} -f %d {cur_input_dir} >> {log_file}"
+                convert_cmd = f"{dcm2niix} -z y -9 -d -v 0 -o {cur_output_dir} -f {session}_{scan} {cur_input_dir} >> {log_file}" # -f %d
                 os.system(log_cmd)
                 os.system(convert_cmd)
             else:
---
+++ code/preprocessing/2b_parse_logfile_warnings.py
@@ -0,0 +1,63 @@
+# File: 2b_parse_logfile_warnings.py
+# Date: 01/12/2024
+# Author: Lawrence Chillrud <chili@u.northwestern.edu>
+# Description: Goes through all of the warnings present in 2_NIFTI/log.txt
+
+#--------------------------#
+####      CONTENTS      ####
+#--------------------------#
+# N. Notes
+# 1. Set up filepaths
+# 2. Parse log file
+
+#--------------------------#
+####      N. NOTES      ####
+#--------------------------#
+# This script is meant to go thru all warnings present in 2_NIFTI/log.txt,
+# generating a list of all unique warnings present in the log file. To see all
+# warnings, not just those that are most concerning, comment out the warnings in
+# the ignore_warnings list below.
+#
+# This script relies on the following file(s) as inputs:
+#   * data/preprocessing/output/2_NIFTI/log.txt
+
+#---------------------------#
+#### 1. SET UP FILEPATHS ####
+#---------------------------#
+import os
+
+if not os.getcwd().endswith('Meningioma'): os.chdir('../..')
+if not os.getcwd().endswith('Meningioma'): 
+    raise Exception('Please run this script from the Menigioma directory')
+
+#-------------------------#
+#### 2. PARSE LOG FILE ####
+#-------------------------#
+# List of warnings to be ignored
+ignore_warnings = [
+    'Warning: Siemens XA exported as classic not enhanced DICOM (issue 236)',
+    'Warning: Siemens MoCo? Bogus slice timing'
+]
+
+def should_ignore(warning):
+    return any(warning.startswith(ignore_warning) for ignore_warning in ignore_warnings)
+
+def process_file(filename):
+    unique_warnings = []
+    current_converting = ""
+
+    with open(filename, 'r') as file:
+        for line in file:
+            if line.startswith("Converting"):
+                current_converting = line.strip()
+            if line.startswith("Warning"):
+                warning = line.strip()
+                if not should_ignore(warning) and (current_converting, warning) not in unique_warnings:
+                    unique_warnings.append((current_converting, warning))
+
+    return unique_warnings
+
+filename = "data/preprocessing/output/2_NIFTI/log.txt"
+warnings_found = process_file(filename)
+for item in warnings_found:
+    print(item)
