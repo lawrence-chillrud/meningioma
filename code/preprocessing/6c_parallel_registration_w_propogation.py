1162d6edec0cd336f9c5fe85136b8f82218241ec
Lawrence Chillrud <lgc2139@columbia.edu>
2024-02-10 18:12:32 -0600

    Attempt 2 at parallel processing reg

--- code/preprocessing/6c_parallel_registration_w_propogation.py
+++ code/preprocessing/6c_parallel_registration_w_propogation.py
@@ -30,7 +30,7 @@
 #--------------------------#
 #### 0. PACKAGE IMPORTS ####
 #--------------------------#
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, as_completed
 from utils import setup, lsdir, read_example_mri
 from datetime import datetime
 import time
@@ -38,6 +38,7 @@ import shutil
 import os
 import ants
 import logging
+from tqdm import tqdm
 
 #-------------------------#
 #### 1. FILE WRANGLING ####
@@ -63,22 +64,13 @@ if not os.path.exists(rigid_template_path):
 
 mni_template = ants.image_read(rigid_template_path, reorient='IAL')
 
-overall_log_file = os.path.join(output_dir, 'log.txt')
-print(f"Logging output to {overall_log_file}")
-overall_begin_time = time.time()
-overall_start_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-bar = '-' * 80
-os.system(f"echo '\n{bar}\n' >> {overall_log_file}")
-os.system(f"echo 'Running script 6c_parallel_registration_w_propogation.py at {overall_start_time}\n' >> {overall_log_file}")
-os.system(f"echo 'Skull stripping directory used: {skullstrip_dir}' >> {overall_log_file}")
-os.system(f"echo 'Nonlinear template used: {nonlinear_template}' >> {overall_log_file}")
-os.system(f"echo 'Rigid template used: {rigid_template_path}\n' >> {overall_log_file}")
-
 #-----------------------#
 #### 2. REGISTRATION ####
 #-----------------------#
 def register_subject(subject):
     log_file = os.path.join(log_dir, f'{subject}-log.txt')
+    logging.basicConfig(filename=log_file, level=logging.INFO, format='%(message)s')
+    
     begin_time = time.time()
 
     for session in lsdir(f'{data_dir}/{subject}'):
@@ -100,10 +92,10 @@ def register_subject(subject):
         if 'SAG_3D_T2' in scan_types: has_t2 = True
 
         if not has_post:
-            os.system(f"echo 'Warning: No AX_3D_T1_POST scan found for {session}, therefore skipping session: {session}' >> {log_file}")
+            logging.info(f"Warning: No AX_3D_T1_POST scan found for {session}, therefore skipping session: {session}")
             continue
         else:
-            os.system(f"echo 'Starting registration for the session: {session}' >> {log_file}")
+            logging.info(f"Starting registration for the session: {session}")
 
         pre_req_scans = current_scans
         pre_req_scans = [s for s in pre_req_scans if not s.endswith('AX_3D_T1_POST')]
@@ -129,13 +121,13 @@ def register_subject(subject):
         ax3dt1post_path = [s for s in current_scans if s.endswith(nonlinear_template)][0]
         ax3dt1post = read_example_mri(data_dir, subject, session, ax3dt1post_path, ants=True, orientation='IAL')
         if ax3dt1post.spacing != (1.0, 1.0, 1.0):
-            os.system(f"echo '\tWarning: {session}/{ax3dt1post_path} does not have 1x1x1mm spacing. Instead it has: {ax3dt1post.spacing}' >> {log_file}")
+            logging.info(f"\tWarning: {session}/{ax3dt1post_path} does not have 1x1x1mm spacing. Instead it has: {ax3dt1post.spacing}")
         
         swi_intermediary_transform = None
         dwi_intermediary_transform = None
 
         type_of_transform = 'Rigid'
-        os.system(f"echo '\tPerforming {type_of_transform.lower()} registration to {rigid_template_path.split('/')[-1]} for scan {nonlinear_template}' >> {log_file}")
+        logging.info(f"\tPerforming {type_of_transform.lower()} registration to {rigid_template_path.split('/')[-1]} for scan {nonlinear_template}")
         rigid_transform = ants.registration(
             fixed=mni_template,
             moving=ax3dt1post,
@@ -148,19 +140,19 @@ def register_subject(subject):
         rigid_transform['warpedmovout'].to_file(f'{cur_output_dir}/{session}_{ax3dt1post_path}.nii.gz')
         shutil.copy(f'{data_dir}/{subject}/{session}/{ax3dt1post_path}/{session}_{ax3dt1post_path}.json', f'{cur_output_dir}/{session}_{ax3dt1post_path}.json')
 
-        os.system(f"echo '\tNonlinear warping the pre-requisite scans {pre_req_scans} to {ax3dt1post_path}, then propogating rigid registration ({ax3dt1post_path} -> {rigid_template_path.split('/')[-1]}) onto each scan' >> {log_file}")
+        logging.info(f"\tNonlinear warping the pre-requisite scans {pre_req_scans} to {ax3dt1post_path}, then propogating rigid registration ({ax3dt1post_path} -> {rigid_template_path.split('/')[-1]}) onto each scan")
         for scan in pre_req_scans:
             cur_input_dir = f'{data_dir}/{subject}/{session}/{scan}'
             cur_output_dir = f'{output_dir}/{subject}/{session}/{scan}'
             scan_type = scan.split('-')[-1]
             if not os.path.exists(f'{cur_output_dir}/{session}_{scan}.nii.gz'): 
-                os.system(f"echo '\t\tProcessing: {scan}' >> {log_file}")
+                logging.info(f"\t\tProcessing: {scan}")
                 if not os.path.exists(cur_output_dir): os.makedirs(cur_output_dir)
                 shutil.copy(f'{cur_input_dir}/{session}_{scan}.json', f'{cur_output_dir}/{session}_{scan}.json')
                 original_mri = read_example_mri(data_dir, subject, session, scan, ants=True, orientation='IAL')
 
                 try:
-                    os.system(f"echo '\t\t\tNonlinear warping {scan} to {ax3dt1post_path}' >> {log_file}")
+                    logging.info(f"\t\t\tNonlinear warping {scan} to {ax3dt1post_path}")
                     type_of_transform = 'SyNRA'
                     nonlinear_transform = ants.registration(
                         fixed=ax3dt1post,
@@ -172,7 +164,7 @@ def register_subject(subject):
                     if scan_type == swi_intermediary: swi_intermediary_transform = nonlinear_transform['fwdtransforms']
                     if scan_type == dwi_intermediary: dwi_intermediary_transform = nonlinear_transform['fwdtransforms']
 
-                    os.system(f"echo '\t\t\tPropogating the rigid registration from {ax3dt1post_path} -> {rigid_template_path.split('/')[-1]} onto {scan}' >> {log_file}")
+                    logging.info(f"\t\t\tPropogating the rigid registration from {ax3dt1post_path} -> {rigid_template_path.split('/')[-1]} onto {scan}")
                     propogated_rigid_transform = ants.apply_transforms(
                         fixed=mni_template,
                         moving=nonlinear_transform['warpedmovout'],
@@ -183,10 +175,10 @@ def register_subject(subject):
                     propogated_rigid_transform.to_file(f'{cur_output_dir}/{session}_{scan}.nii.gz')
 
                 except Exception as e:
-                    os.system(f"echo '\t\t\tError: {e}' >> {log_file}")
-                    os.system(f"echo '\t\t\tFixed shape: {ax3dt1post.numpy().shape}' >> {log_file}")
-                    os.system(f"echo '\t\t\tMoving shape: {original_mri.numpy().shape}' >> {log_file}")
-                    os.system(f"echo '\t\t\tUnable to register {scan}\n' >> {log_file}")
+                    logging.info(f"\t\t\tError: {e}")
+                    logging.info(f"\t\t\tFixed shape: {ax3dt1post.numpy().shape}")
+                    logging.info(f"\t\t\tMoving shape: {original_mri.numpy().shape}")
+                    logging.info(f"\t\t\tUnable to register {scan}\n")
         
         swi_dwi_scans = []
         if has_swi:
@@ -197,13 +189,13 @@ def register_subject(subject):
             swi_dwi_scans.append(dwi_path)
         
         if len(swi_dwi_scans) > 0:
-            os.system(f"echo '\tNonlinear warping {swi_dwi_scans} scans to {ax3dt1post_path} then propogating rigid registration ({ax3dt1post_path} -> {rigid_template_path.split('/')[-1]}) onto each scan' >> {log_file}")
+            logging.info(f"\tNonlinear warping {swi_dwi_scans} scans to {ax3dt1post_path} then propogating rigid registration ({ax3dt1post_path} -> {rigid_template_path.split('/')[-1]}) onto each scan")
             for scan in swi_dwi_scans:
                 cur_input_dir = f'{data_dir}/{subject}/{session}/{scan}'
                 cur_output_dir = f'{output_dir}/{subject}/{session}/{scan}'
                 scan_type = scan.split('-')[-1]
                 if not os.path.exists(f'{cur_output_dir}/{session}_{scan}.nii.gz'): 
-                    os.system(f"echo '\t\tProcessing: {scan}' >> {log_file}")
+                    logging.info(f"\t\tProcessing: {scan}")
                     if not os.path.exists(cur_output_dir): os.makedirs(cur_output_dir)
                     shutil.copy(f'{cur_input_dir}/{session}_{scan}.json', f'{cur_output_dir}/{session}_{scan}.json')
                     original_mri = read_example_mri(data_dir, subject, session, scan, ants=True, orientation='IAL')
@@ -219,7 +211,7 @@ def register_subject(subject):
 
                     intermediary_dir = data_dir
                     if skullstrip_dir: intermediary_dir = skullstrip_dir
-                    os.system(f"echo '\t\t\tSubstep 1/3: Nonlinear warping {scan} to {intermediary_path} (from {intermediary_dir})' >> {log_file}")
+                    logging.info(f"\t\t\tSubstep 1/3: Nonlinear warping {scan} to {intermediary_path} (from {intermediary_dir})")
                     intermediary_mri = read_example_mri(intermediary_dir, subject, session, intermediary_path, ants=True, orientation='IAL')
                     
                     type_of_transform = 'SyNRA'
@@ -232,7 +224,7 @@ def register_subject(subject):
                         )
                         nonlinear_int_transform['warpedmovout'].to_file(f'{cur_output_dir}/{session}_{scan}_{type_of_transform}_registration_to_{intermediary}.nii.gz')
 
-                        os.system(f"echo '\t\t\tSubstep 2/3: Propogating the nonlinear warp from {intermediary_path} -> {ax3dt1post_path} onto {scan}' >> {log_file}")
+                        logging.info(f"\t\t\tSubstep 2/3: Propogating the nonlinear warp from {intermediary_path} -> {ax3dt1post_path} onto {scan}")
                         # propogate intermediary transform
                         nonlinear_transform = ants.apply_transforms(
                             fixed=ax3dt1post,
@@ -243,7 +235,7 @@ def register_subject(subject):
 
                         nonlinear_transform.to_file(f'{cur_output_dir}/{session}_{scan}_{type_of_transform}_propogated_registration_using_transform_from_{intermediary_path}_to_{nonlinear_template}.nii.gz')
 
-                        os.system(f"echo '\t\t\tSubstep 3/3: Propogating the rigid registration from {ax3dt1post_path} -> {rigid_template_path.split('/')[-1]} onto {scan}' >> {log_file}")
+                        logging.info(f"\t\t\tSubstep 3/3: Propogating the rigid registration from {ax3dt1post_path} -> {rigid_template_path.split('/')[-1]} onto {scan}")
                         propogated_rigid_transform = ants.apply_transforms(
                             fixed=mni_template,
                             moving=nonlinear_transform,
@@ -254,29 +246,54 @@ def register_subject(subject):
                         propogated_rigid_transform.to_file(f'{cur_output_dir}/{session}_{scan}.nii.gz')
 
                     except Exception as e:
-                        os.system(f"echo '\t\t\tError: {e}' >> {log_file}")
-                        os.system(f"echo '\t\t\tFixed shape: {intermediary_mri.numpy().shape}' >> {log_file}")
-                        os.system(f"echo '\t\t\tMoving shape: {original_mri.numpy().shape}' >> {log_file}")
-                        os.system(f"echo '\t\t\tUnable to register {scan}\n' >> {log_file}")
+                        logging.info(f"\t\t\tError: {e}")
+                        logging.info(f"\t\t\tFixed shape: {intermediary_mri.numpy().shape}")
+                        logging.info(f"\t\t\tMoving shape: {original_mri.numpy().shape}")
+                        logging.info(f"\t\t\tUnable to register {scan}\n")
 
         time_elapsed = time.time() - begin_time
-        os.system(f"echo 'Completed registration for the session: {session}' >> {log_file}")
+        logging.info(f"Completed registration for the session: {session}")
         hours, rem = divmod(time_elapsed, 3600)
         minutes, seconds = divmod(rem, 60)
         time_elapsed = "{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds)
-        os.system(f"echo 'Elapsed time: {time_elapsed}\n' >> {log_file}")
+        logging.info(f"Elapsed time: {time_elapsed}\n")
 
-subjects = lsdir(data_dir)
-for subject in subjects:
+def main():
+    # start overall logging
+    overall_log_file = os.path.join(output_dir, 'log.txt')
+    print(f"Logging output to {overall_log_file}")
+    overall_begin_time = time.time()
+    overall_start_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    bar = '-' * 80
+    os.system(f"echo '\n{bar}\n' >> {overall_log_file}")
+    os.system(f"echo 'Running script 6c_parallel_registration_w_propogation.py at {overall_start_time}\n' >> {overall_log_file}")
+    os.system(f"echo 'Skull stripping directory used: {skullstrip_dir}' >> {overall_log_file}")
+    os.system(f"echo 'Nonlinear template used: {nonlinear_template}' >> {overall_log_file}")
+    os.system(f"echo 'Rigid template used: {rigid_template_path}\n' >> {overall_log_file}")
+
+    # register all subjects!!
+    subjects = lsdir(data_dir)
     with ProcessPoolExecutor(max_workers=4) as executor:
-        executor.submit(register_subject, subject)
+        # Create a list of futures
+        futures = [executor.submit(register_subject, subject) for subject in subjects]
+        
+        # Initialize tqdm progress bar
+        with tqdm(total=len(futures)) as progress_bar:
+            for future in as_completed(futures):
+                progress_bar.update(1)
+                # Result of the future can be obtained if needed
+                # result = future.result()
 
-overall_end_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-overall_time_elapsed = time.time() - overall_begin_time
-hours, rem = divmod(overall_time_elapsed, 3600)
-minutes, seconds = divmod(rem, 60)
-overall_time_elapsed = "{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds)
-os.system(f"cat {log_dir}/* >> {overall_log_file}")
-os.system(f"echo '\nCompleted registration for all subjects at {overall_end_time}' >> {overall_log_file}")
-os.system(f"echo 'Total elapsed time: {overall_time_elapsed}\n' >> {overall_log_file}")
-os.system(f"echo '{bar}\n' >> {overall_log_file}")
+    # end overall logging
+    overall_end_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    overall_time_elapsed = time.time() - overall_begin_time
+    hours, rem = divmod(overall_time_elapsed, 3600)
+    minutes, seconds = divmod(rem, 60)
+    overall_time_elapsed = "{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds)
+    os.system(f"cat {log_dir}/* >> {overall_log_file}")
+    os.system(f"echo '\nCompleted registration for all subjects at {overall_end_time}' >> {overall_log_file}")
+    os.system(f"echo 'Total elapsed time: {overall_time_elapsed}\n' >> {overall_log_file}")
+    os.system(f"echo '{bar}\n' >> {overall_log_file}")
+
+if __name__ == '__main__':
+    main()
