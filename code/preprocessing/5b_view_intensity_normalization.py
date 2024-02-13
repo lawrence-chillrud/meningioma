cca09e9094bdd3bf9c520b8ce4e941067288a144
Lawrence Chillrud <lgc2139@columbia.edu>
2024-02-13 04:28:37 -0600

    added prog bar to 5b

--- code/preprocessing/5b_view_intensity_normalization.py
+++ code/preprocessing/5b_view_intensity_normalization.py
@@ -18,6 +18,7 @@
 from utils import setup, lsdir, read_example_mri, plot_histogram
 import matplotlib.pyplot as plt
 import numpy as np
+from tqdm import tqdm
 
 setup()
 
@@ -46,7 +47,7 @@ def view_intensity_normalization(data_dir='data/preprocessing/output', scan_type
         The height of the figure. The default is 6.
     """
     before_dir = f'{data_dir}/3_N4_BIAS_FIELD_CORRECTED'
-    after_dir = f'{data_dir}/5_ZSCORE_NORMALIZED'
+    after_dir = f'{data_dir}/5b_ZSCORE_NORMALIZED'
     subjects = lsdir(after_dir)
     subjects = [s for s in subjects if f'{s}_Brainlab' in lsdir(f'{after_dir}/{s}')]
 
@@ -124,6 +125,6 @@ def view_intensity_normalization(data_dir='data/preprocessing/output', scan_type
 # view_intensity_normalization()
 
 # %% 
-for st in ['AX_3D_T1_POST', 'AX_3D_T1_PRE', 'AX_ADC', 'AX_DIFFUSION', 'SAG_3D_FLAIR', 'AX_SWI', 'SAG_3D_T2']:
+for st in tqdm(['AX_3D_T1_POST', 'AX_3D_T1_PRE', 'AX_ADC', 'AX_DIFFUSION', 'SAG_3D_FLAIR', 'AX_SWI', 'SAG_3D_T2']):
     view_intensity_normalization(scan_type=st, subjects_to_plot=['105', '110', '111', '115'], title="Z-score Normalization", save_fig=True)
 # %%
