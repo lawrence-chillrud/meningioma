fc668e8be2bef61a0df9d172f23c4bc90a68e6cc
Lawrence Chillrud <lgc2139@columbia.edu>
2024-03-16 15:51:30 -0500

    Finished extract features script for now, can take multiple seg files

--- code/radiomics/1a_extract_features.py
+++ code/radiomics/1a_extract_features.py
@@ -59,19 +59,67 @@ LOG_FILE = f'{OUTPUT_DIR}/log.txt'
 MODALITIES = ['AX_3D_T1_POST', 'AX_DIFFUSION', 'AX_ADC', 'SAG_3D_FLAIR']
 if not os.path.exists(OUTPUT_DIR): os.makedirs(OUTPUT_DIR)
 
+def assert_images_geometry(sitk_images, paths):
+    # Assuming sitk_images is a list of SimpleITK.Image objects
+    if not sitk_images: raise ValueError("The list of segmentations is empty.")
+
+    first_image = sitk_images[0]
+    first_origin = first_image.GetOrigin()
+    first_spacing = first_image.GetSpacing()
+    first_direction = first_image.GetDirection()
+
+    if len(sitk_images) == 1:
+        return first_origin, first_spacing, first_direction
+    
+    for i, img in enumerate(sitk_images[1:], start=1):
+        if not (img.GetOrigin() == first_origin and
+                img.GetSpacing() == first_spacing and
+                img.GetDirection() == first_direction):
+            raise AssertionError(f"Multiple segmentation files found: {paths}. Segmentation at index {i} has different geometry from the segmentation at index 0. This is a problem!")
+    
+    return first_origin, first_spacing, first_direction
+
 def extract_features(s, e):
     logging.info(f'\nExtracting features for subject {s}...')
-    seg_path = SEGS_DIR + [f for f in SEGS_PATHS if f.startswith(f'Segmentation {s}.nii')][0]
-    seg = sitk.ReadImage(seg_path)
-    seg_whole_tumor = sitk.BinaryThreshold(seg, lowerThreshold=1, insideValue=22)
-    segmentation_labels = [int(v) for v in np.unique(sitk.GetArrayFromImage(seg)) if v != 0]
-    if len(segmentation_labels) > 1: segmentation_labels.append(22)
+
+    all_seg_paths = [f for f in SEGS_PATHS if (f.startswith(f'Segmentation {s}.nii') or f.startswith(f'Segmentation {s} '))]
+    all_seg_sitks = []
+    all_seg_arrays = []
+    all_seg_labels = []
+    for sp in all_seg_paths:
+        seg = sitk.ReadImage(SEGS_DIR + sp)
+        all_seg_sitks.append(seg)
+        seg_arr = sitk.GetArrayFromImage(seg)
+        all_seg_arrays.append(seg_arr)
+        all_seg_labels.extend([int(v) for v in np.unique(seg_arr) if v != 0])
+
+    origin, spacing, direction = assert_images_geometry(all_seg_sitks, all_seg_paths)
+
+    all_seg_labels = sorted(list(set(all_seg_labels)))
+    all_seg_labels.append(22)
+    
+    masks = []
+    for lab in all_seg_labels:
+        mask = np.zeros_like(all_seg_arrays[0])
+        for seg_arr in all_seg_arrays:
+            if lab == 22:
+                mask = np.logical_or(mask > 0, seg_arr > 0)
+                mask = mask.astype(int) * 22
+            else:
+                mask = np.logical_or(mask == lab, seg_arr == lab)
+                mask = mask.astype(int) * lab
+        mask_sitk = sitk.GetImageFromArray(mask)
+        mask_sitk.SetOrigin(origin)
+        mask_sitk.SetSpacing(spacing)
+        mask_sitk.SetDirection(direction)
+        masks.append(mask_sitk)
+        
     session = f'{s}_Brainlab'
     if len(lsdir(f'{MRI_DIR}/{s}')) > 1:
         session = lsdir(f'{MRI_DIR}/{s}')[0]
         logging.warning(f'More than one session for {s}, using the first one')
 
-    s_modalities_paths = lsdir(f'{MRI_DIR}/{s}/{session}')    
+    s_modalities_paths = lsdir(f'{MRI_DIR}/{s}/{session}')
     s_modalities = [m.split('-')[-1] for m in s_modalities_paths]
 
     for modality in MODALITIES:
@@ -82,12 +130,9 @@ def extract_features(s, e):
         modality_path = s_modalities_paths[s_modalities.index(modality)]
         mri_path = f'{MRI_DIR}/{s}/{session}/{modality_path}/{session}_{modality_path}.nii.gz'
         
-        for seg_lab in segmentation_labels:
+        for i in range(len(all_seg_labels)):
             try:
-                if seg_lab == 22:
-                    result = e.execute(mri_path, seg_whole_tumor, label=seg_lab)
-                else: 
-                    result = e.execute(mri_path, seg_path, label=seg_lab)
+                result = e.execute(mri_path, masks[i], label=all_seg_labels[i])
                 result_trimmed = result.copy()
                 for k in result.keys():
                     if not isinstance(result[k], np.ndarray):
@@ -95,16 +140,17 @@ def extract_features(s, e):
                     elif result[k].size > 1:
                         del result_trimmed[k]
                 
-                features_row = pd.concat([pd.DataFrame({'Subject Number': [s], 'Modality': [modality], 'Segmentation Label': seg_lab}), pd.Series(result_trimmed).to_frame().T], axis=1)
+                features_row = pd.concat([pd.DataFrame({'Subject Number': [s], 'Modality': [modality], 'Segmentation Label': all_seg_labels[i]}), pd.Series(result_trimmed).to_frame().T], axis=1)
                 if not os.path.exists(OUTPUT_FILE):
                     features_row.to_csv(OUTPUT_FILE, index=False)
                 else:
                     features_row.to_csv(OUTPUT_FILE, mode='a', index=False, header=False)
             
-                logging.info(f"Subject {s} (label {seg_lab}): Success!\n")
+                logging.info(f"Subject {s} (label {all_seg_labels[i]}): Success!\n")
             except Exception as _:
                 logging.error(f"Subject {s}: FAILED\n")
                 continue
+
 def main():
     _, _, labels_df = count_subjects(verbose=False)
     subjects = labels_df['Subject Number'].to_list()
@@ -117,7 +163,7 @@ def main():
     logging.info(bar)
     logging.info(f'Starting radiomics feature extraction for n = {n} subjects at {overall_start_time}')
 
-    radiomics.setVerbosity(logging.INFO)
+    radiomics.setVerbosity(level=60) # logging.INFO or level=60
     # handler = logging.FileHandler(LOG_FILE, 'a')
     # formatter = logging.Formatter("%(levelname)s:%(name)s: %(message)s")
     # handler.setFormatter(formatter)
@@ -134,6 +180,17 @@ def main():
     
     for subject in tqdm(subjects):
         extract_features(subject, extractor)
+    
+    logging.info('Pivoting features to wide format...')
+    df = pd.read_csv(OUTPUT_FILE)
+    df_wide = df.pivot(
+        index='Subject Number', 
+        columns=['Modality', 'Segmentation Label']
+    )
+    df_wide.columns = [f"Mod-{modality}-SegLab-{segmentation_label}-Feat-{feature}" for (feature, modality, segmentation_label) in df_wide.columns]
+    logging.info(f'Saving wide features (shape={df_wide.shape}) to csv file...')
+    df_wide.to_csv(f'{OUTPUT_DIR}/features_wide.csv')
+    logging.info('Done!\n')
     
     overall_end_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     overall_time_elapsed = time.time() - overall_begin_time
