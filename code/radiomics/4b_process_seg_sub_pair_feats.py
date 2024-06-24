0faa003020c9dd2a50449b9e6ba7aa194b0f16a1
Lawrence Chillrud <lgc2139@columbia.edu>
2024-06-24 14:37:08 -0500

    median post processing of seg sub pairs done

---
+++ code/radiomics/4b_process_seg_sub_pair_feats.py
@@ -0,0 +1,45 @@
+# %%
+import sys
+import os
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+
+import numpy as np
+import pandas as pd
+from preprocessing.utils import setup
+
+setup()
+
+def median_abs_deviation(series):
+    return np.nanmedian(np.abs(series - np.nanmedian(series)))
+
+df = pd.read_csv('data/4a_radiomics_for_all_seg_sub_pairs/features.csv')
+
+# %%
+groups = df.drop(columns=['Subject Providing Scan']).groupby(['Subject Providing Segmentation', 'Segmentation Label', 'Scan Sequence'])
+
+# Calculate the median for each group
+medians = groups.median().reset_index().pivot(
+        index='Subject Providing Segmentation', 
+        columns=['Scan Sequence', 'Segmentation Label']
+    )
+
+# Apply the median_abs_deviation function to calculate MAD for each group
+mads = groups.apply(lambda g: g.iloc[:, 4:].apply(median_abs_deviation)).reset_index().pivot(
+        index='Subject Providing Segmentation', 
+        columns=['Scan Sequence', 'Segmentation Label']
+    )
+
+# %%
+medians.columns = [f"Mod-{modality}-SegLab-{segmentation_label}-Feat-{feature}" for (feature, modality, segmentation_label) in medians.columns]
+mads.columns = [f"Mod-{modality}-SegLab-{segmentation_label}-Feat-{feature}" for (feature, modality, segmentation_label) in mads.columns]
+
+# %%
+output_dir = 'data/4b_processed_seg_sub_pair_feats'
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+medians.to_csv(f'{output_dir}/medians.csv')
+mads.to_csv(f'{output_dir}/mads.csv')
+# %%
