375812a76247c56b6ab1c4a3f32f3e040e9e0d6f
Lawrence Chillrud <lgc2139@columbia.edu>
2024-08-13 13:27:52 -0400

    Also looking for b500s to remove

--- code/preprocessing/1_remove_b0_from_tracews.py
+++ code/preprocessing/1_remove_b0_from_tracews.py
@@ -54,27 +54,30 @@ def check_for_multiple_scan_types(scan_dir):
     return sorted(list(set(scan_types)))
 
 #%%---------------------------#
-#### 2. MOVE B0 SCANS AWAY ####
+#### 2. MOVE B0/B500 SCANS AWAY ####
 #-----------------------------#
 for subject in tqdm(lsdir(data_dir), desc='Subjects', total=len(lsdir(data_dir)), position=0, smoothing=0, dynamic_ncols=True, colour='white'):
     for session in tqdm(lsdir(f'{data_dir}/{subject}'), desc='Sessions', total=len(lsdir(f'{data_dir}/{subject}')), position=1, smoothing=0, dynamic_ncols=True, colour='green', leave=False):
         for scan in tqdm(lsdir(f'{data_dir}/{subject}/{session}/scans'), desc='Scans', total=len(lsdir(f'{data_dir}/{subject}/{session}/scans')), position=2, smoothing=0, dynamic_ncols=True, colour='red', leave=False):
             scan_types_found = check_for_multiple_scan_types(f'{data_dir}/{subject}/{session}/scans/{scan}/{dcm}')
             b0_found = False
+            b500_found = False
             for scan_type in scan_types_found:
                 if 'b0' in scan_type:
                     b0_found = True
-                    break
-            if len(scan_types_found) > 1 and b0_found:
+                if 'b500' in scan_type:
+                    b500_found = True
+            
+            if len(scan_types_found) > 1 and (b0_found or b500_found):
                 num = scan.split('-')[0]
                 name = scan.split('-')[-1]
-                destination_dir = f'{alt_dir}/{subject}/{session}/scans/{num}-Removed_B0_{name}/{dcm}'
+                destination_dir = f'{alt_dir}/{subject}/{session}/scans/{num}-Removed_B0_B500_{name}/{dcm}'
                 for f in os.listdir(f'{data_dir}/{subject}/{session}/scans/{scan}/{dcm}'):
                     if f.endswith('.dcm'):
                         current_path = f'{data_dir}/{subject}/{session}/scans/{scan}/{dcm}/{f}'
                         dicom = pydicom.dcmread(current_path)
                         if 'SequenceName' in dicom:
-                            if 'b0' in dicom.SequenceName.lower():
+                            if 'b0' in dicom.SequenceName.lower() or 'b500' in dicom.SequenceName.lower():
                                 if not os.path.exists(destination_dir): os.makedirs(destination_dir)
                                 destination_path = f'{destination_dir}/{f}'
                                 os.rename(current_path, destination_path)
@@ -84,30 +87,4 @@ for subject in tqdm(lsdir(data_dir), desc='Subjects', total=len(lsdir(data_dir))
                             continue
                     else:
                         continue
-                if os.path.exists(destination_dir): print(f'Moved B0s into {subject}/{session}/scans/{num}-Removed_B0_{name}/')
-
-# %%
-# ax_diff_count = 0
-# b1000_count = 0
-# for subject in tqdm(lsdir(data_dir), desc='Subjects', total=len(lsdir(data_dir)), position=0, smoothing=0, dynamic_ncols=True, colour='white'):
-#     for session in lsdir(f'{data_dir}/{subject}'):
-#         for scan in lsdir(f'{data_dir}/{subject}/{session}/ready_for_preprocessing'):
-#             if scan.endswith('AX_DIFFUSION'):
-#                 ax_diff_count += 1
-#                 for f in os.listdir(f'{data_dir}/{subject}/{session}/ready_for_preprocessing/{scan}/{dcm}'):
-#                     if f.endswith('.dcm'):
-#                         current_path = f'{data_dir}/{subject}/{session}/ready_for_preprocessing/{scan}/{dcm}/{f}'
-#                         dicom = pydicom.dcmread(current_path)
-#                         if 'b1000' in dicom.SequenceName.lower():
-#                             b1000_count += 1
-#                             break
-#                         else:
-#                             continue
-#                     else:
-#                         continue
-#             else:
-#                 continue
-
-# assert ax_diff_count == b1000_count, f'Found {ax_diff_count} AX_DIFFUSION scans and {b1000_count} b1000 scans.'
-# print('Done!')
-# %%
+                if os.path.exists(destination_dir): print(f'Moved B0/500s into {subject}/{session}/scans/{num}-Removed_B0_B500_{name}/')
