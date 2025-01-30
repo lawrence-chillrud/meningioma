8c281d49d63e3fbce8774dac45a5e8fd8930557e
Lawrence Chillrud <chili@u.northwestern.edu>
2025-01-30 15:34:18 -0600

    implemented getitem and len methods

--- code/deeplearning/prep_data.py
+++ code/deeplearning/prep_data.py
@@ -1,6 +1,7 @@
 # %%
 import os
 if not os.getcwd().endswith('code'): os.chdir('..')
+from deeplearning.utils import get_segs, get_mris
 from preprocessing.utils import lsdir
 from radiomics.utils import plot_data_split
 from torch.utils.data import Dataset, DataLoader
@@ -8,16 +9,39 @@ import pandas as pd
 
 # %%
 class MeningiomaDataset(Dataset):
-    def __init__(self, task_name, mri_dir, seg_dir, labels_file, pulse_sequences=['ADC', 'FLAIR', 'T1_POST', 'DIFFUSION'], transform=None):
+    def __init__(
+        self, 
+        task_name, 
+        labels_file='data/labels/MeningiomaBiomarkerData.csv', 
+        mri_dir='data/preprocessing/output/7b_COMPLETED_PREPROCESSED', 
+        pulse_sequences=['T1_POST', 'FLAIR', 'ADC'], 
+        seg_dir='data/all_smooth_segs_12-12-24/', 
+        seg_rois=[1, 3, 4, 5, 6], 
+        transform=None
+    ):
+        """
+        Parameters
+        ----------
+        task_name (str): Name of the classification task to be performed. Must be a column in the labels file. Can be one of: ['Chr22q', 'MethylationSubgroup', 'Chr1p']
+        labels_file (str): Path to the CSV file containing the labels for each subject.
+        mri_dir (str): Directory containing the MRI images.
+        pulse_sequences (list): List of pulse sequences to include in the dataset.
+        seg_dir (str): Directory containing the segmentation masks. If None, no segmentation masks will be included in the dataset.
+        seg_rois (list): List of region of interest (roi) labels to extract from the segmentation masks.
+        transform (callable, optional): Optional transform to be applied to the data.
+        """
+        # store input arguments
         self.task_name = task_name
-        self.mri_dir = mri_dir
-        self.seg_dir = seg_dir
         self.labels_file = labels_file
+        self.mri_dir = mri_dir
         self.pulse_sequences = pulse_sequences
+        self.seg_dir = seg_dir
+        self.seg_rois = seg_rois
         self.transform = transform
 
         # read in segmentation file paths
-        self.seg_paths = [f for f in os.listdir(self.seg_dir) if f.startswith('Segmentation')]
+        if self.seg_dir is not None:
+            self.seg_paths = [f for f in os.listdir(self.seg_dir) if f.startswith('Segmentation')]
 
         # read in labels file & extract info
         labels_df = pd.read_csv(self.labels_file)
@@ -32,9 +56,12 @@ class MeningiomaDataset(Dataset):
         
         # get list of subjects
         self.subjects_with_mris = [int(s) for s in lsdir(self.mri_dir)]
-        self.subjects_with_segs = [int(s.split(' ')[1].split('.')[0]) for s in self.seg_paths]
         self.subjects_with_labels = self.labels.index
-        self.subjects = sorted(list(set(self.subjects_with_mris) & set(self.subjects_with_segs) & set(self.subjects_with_labels)))
+        if self.seg_dir is not None:
+            self.subjects_with_segs = [int(s.split(' ')[1].split('.')[0]) for s in self.seg_paths]
+            self.subjects = sorted(list(set(self.subjects_with_mris) & set(self.subjects_with_segs) & set(self.subjects_with_labels)))
+        else:
+            self.subjects = sorted(list(set(self.subjects_with_mris) & set(self.subjects_with_labels)))
 
         # see subject availability by session and pulse sequence type
         self.subjects_by_session = {'brainlab': [], 'presurgical': [], 'other': []}
@@ -64,34 +91,53 @@ class MeningiomaDataset(Dataset):
         self.subjects = sorted(list(subjects_set))
         for session in self.subjects_by_session:
             self.subjects_by_session[session] = sorted(list(set(self.subjects_by_session[session]) & subjects_set))
+        
+    def __len__(self):
+        return len(self.subjects)
 
+    def __getitem__(self, idx):
+        # get subject ID
+        sub_id = self.subjects[idx]
+
+        # get session info
+        session = lsdir(f'{self.mri_dir}/{sub_id}')[0]
+        session_type = session.split('_')[-1].lower()
+
+        # get mris
+        mris = get_mris(subject_mri_dir=f'{self.mri_dir}/{sub_id}/{session}', pulse_sequences=self.pulse_sequences)
+        if self.transform: mris = self.transform(mris)
+
+        # get label
+        label = self.labels[sub_id]
+
+        # get segmentations
+        if self.seg_dir is not None:
+            segs = get_segs(subject=sub_id, seg_dir=self.seg_dir, seg_paths=self.seg_paths, rois=self.seg_rois)
+            if self.transform: segs = self.transform(segs)
+            return mris, segs, label, sub_id, session_type
+        
+        return mris, label, sub_id, session_type
+    
+    def plot_data_split(self):
         plot_data_split(self.labels[self.subjects].values.astype(int), title=f"All subjects {self.task_name}")
         plot_data_split(self.labels[self.subjects_by_session['brainlab']].values.astype(int), title=f"Brainlab subjects {self.task_name}")
         plot_data_split(self.labels[self.subjects_by_session['presurgical']].values.astype(int), title=f"Presurgical subjects {self.task_name}")
 
-    def __len__(self):
-        # return len(self.data)
-        raise NotImplementedError
-
-    def __getitem__(self, idx):
-        # return self.data[idx], self.labels[idx]
-        raise NotImplementedError
-    
     def get_labels(self):
         return self.labels
 
     def get_subjects(self):
-        return self.subjects, self.subjects_with_mris, self.subjects_with_segs, self.subjects_with_labels
+        if self.seg_dir is not None:
+            return self.subjects, self.subjects_with_mris, self.subjects_with_segs, self.subjects_with_labels
+        return self.subjects, self.subjects_with_mris, self.subjects_with_labels
     
 # %%
 if not os.getcwd().endswith('Meningioma'): os.chdir('..')
 
 ds = MeningiomaDataset(
     task_name='Chr22q',
-    mri_dir='data/preprocessing/output/7b_COMPLETED_PREPROCESSED',
-    seg_dir='data/all_smooth_segs_12-12-24',
-    labels_file='data/labels/MeningiomaBiomarkerData.csv',
-    pulse_sequences=['t1_post'],
-    transform=None
+    pulse_sequences=['t1_post']
 )
+# %%
+mris, segs, label, sub_id, session_type = ds[0]
 # %%
---
+++ code/deeplearning/utils.py
@@ -0,0 +1,137 @@
+import numpy as np
+from ants import image_read
+from preprocessing.utils import lsdir
+
+def get_segs(subject, seg_dir, seg_paths, rois=[1, 3, 4, 5, 6]):
+    """
+    Given a subject ID number, returns volumetric segmentation mask for the specified region of interest (roi).
+
+    Parameters:
+    -----------
+    subject (str or int): The subject ID number.
+    seg_dir (str): The directory containing the segmentation masks.
+    seg_paths (list): List of segmentation mask file names.
+    roi (int): The region of interest (roi) to extract from the segmentation mask. Default is 22 (whole tumor mask).
+
+    Returns:
+    --------
+    (np.ndarray or None): The volumetric segmentation mask for the specified region of interest (roi), 
+    or None if the subject has no segmentation available or lacks the given roi. 
+    Returned mask is binary, with 1s indicating the presence of the roi and 0s elsewhere.
+
+    ROI Key:
+    --------
+    1: Enhancing tumor*
+    2: Other tumor
+    3: Necrotic tumor*
+    4: Edema*
+    5: Susceptibility*
+    6: Resitricted diffusion*
+    7: Normal-appearing white matter (NAWM)
+    13: Enhancing tumor + Necrotic tumor
+    15: Enhancing tumor + Susceptibility
+    16: Enhancing tumor + Resitricted diffusion
+    156: Enhancing tumor + Susceptibility + Resitricted diffusion
+    22: Whole tumor mask
+    """
+    if not seg_dir.endswith('/'): seg_dir += '/'
+    all_seg_paths = [f for f in seg_paths if (f.startswith(f'Segmentation {subject}.nii') or f.startswith(f'Segmentation {subject} '))]
+    if len(all_seg_paths) == 0: return None
+    all_seg_arrays = []
+    all_seg_labels = []
+    for sp in all_seg_paths:
+        seg_arr = image_read(seg_dir + sp, reorient='IAL').numpy()
+        all_seg_arrays.append(seg_arr)
+        all_seg_labels.extend([int(v) for v in np.unique(seg_arr) if v != 0])
+
+    all_seg_labels = sorted(list(set(all_seg_labels)))
+    
+    # Check to see if subject has enhancing and [(necrotic=3), (resitricted diffusion=6)] segmentations, if so, add appropriate labels (13/16) to the list
+    if 1 in all_seg_labels:
+        if 3 in all_seg_labels:
+            all_seg_labels.append(13)
+        if 5 in all_seg_labels:
+            all_seg_labels.append(15)
+        if 6 in all_seg_labels:
+            all_seg_labels.append(16)
+            if 5 in all_seg_labels:
+                all_seg_labels.append(156)
+
+    all_seg_labels = sorted(list(set(all_seg_labels)))
+    all_seg_labels.append(22) # Add the whole tumor mask label
+
+    # Create list of masks, one for each segmentation label
+    masks = []
+    for lab in all_seg_labels:
+        mask = np.zeros_like(all_seg_arrays[0])
+        for seg_arr in all_seg_arrays:
+            if lab == 22:
+                mask = np.logical_or(mask > 0, np.logical_and(seg_arr > 0, seg_arr != 7)) # we want to exclude the NAWM label = 7
+                mask = mask.astype(int) * 22
+            elif lab == 13:
+                mask = np.logical_or(mask == 13, seg_arr == 1)
+                mask = mask.astype(int) * 13
+                mask = np.logical_or(mask == 13, seg_arr == 3)
+                mask = mask.astype(int) * 13
+            elif lab == 15:
+                mask = np.logical_or(mask == 15, seg_arr == 1)
+                mask = mask.astype(int) * 15
+                mask = np.logical_or(mask == 15, seg_arr == 5)
+                mask = mask.astype(int) * 15
+            elif lab == 16:
+                mask = np.logical_or(mask == 16, seg_arr == 1)
+                mask = mask.astype(int) * 16
+                mask = np.logical_or(mask == 16, seg_arr == 6)
+                mask = mask.astype(int) * 16
+            elif lab == 156:
+                mask = np.logical_or(mask == 156, seg_arr == 1)
+                mask = mask.astype(int) * 156
+                mask = np.logical_or(mask == 156, seg_arr == 5)
+                mask = mask.astype(int) * 156
+                mask = np.logical_or(mask == 156, seg_arr == 6)
+                mask = mask.astype(int) * 156
+            else:
+                mask = np.logical_or(mask == lab, seg_arr == lab)
+                mask = mask.astype(int) * lab
+        
+        masks.append(mask)
+
+    # grab those masks that correspond to the requested rois
+    final_masks = {}
+    for roi in rois:
+        if roi not in all_seg_labels:
+            final_masks[roi] = np.zeros_like(masks[0])
+        else:
+            roi_idx = all_seg_labels.index(roi)
+            mask_oi = masks[roi_idx]
+            mask_oi = mask_oi > 0
+            final_masks[roi] = mask_oi.astype(int)
+
+    return final_masks
+
+def get_mris(subject_mri_dir, pulse_sequences):
+    session = subject_mri_dir.split('/')[-1]
+    mris = {}
+    scans = lsdir(subject_mri_dir)
+    for ps in pulse_sequences:
+        for scan in scans:
+            if scan.lower().endswith(ps.lower()):
+                mris[ps] = image_read(f'{subject_mri_dir}/{scan}/{session}_{scan}.nii.gz', reorient='IAL').numpy()
+                break
+    return mris
+
+def find_max_cancerous_slice(seg, axis=0):
+    """
+    Given a volumetric segmentation mask, returns the index of the slice along the provided axis with the maximum tumor content.
+
+    Parameters:
+    -----------
+    seg (np.ndarray): The volumetric segmentation mask.
+    axis (int): The axis along which to find the slice with the maximum tumor content. Default is 0 (axial plane if segmentation loaded in 'IAL' orientation with antspy).
+
+    Returns:
+    --------
+    (int): The index of the slice with the maximum tumor content along the provided axis.
+    """
+    axes_to_sum_over = tuple(set(np.arange(seg.ndim)) - {axis})
+    return np.argmax(np.sum(seg, axis=axes_to_sum_over))
