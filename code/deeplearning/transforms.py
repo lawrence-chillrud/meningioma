906f4e825b69ff9aebab9fcb3bbaf59f4f1c7d79
Lawrence Chillrud <chili@u.northwestern.edu>
2025-02-28 17:30:38 -0600

    CubifyVolume tx added

--- code/deeplearning/transforms.py
+++ code/deeplearning/transforms.py
@@ -1,9 +1,64 @@
 import torch
 from torch.nn.functional import pad, interpolate
 
+class CubifyVolume(object):
+    """
+    Resizes all the volumes of a sample to be of shape [cube_size x cube_size x cube_size]
+    """
+    def __init__(self, cube_size=96):
+        self.cube_size = cube_size
+    
+    def __repr__(self):
+        return f"{self.__class__.__name__}(cube_size={self.cube_size})"
+
+    def __call__(self, sample):
+        # get mris and (when available) segs dicts from the input sample
+        mris = sample['mris']
+        if 'segs' in sample.keys():
+            segs = sample['segs']
+        else:
+            segs = None
+        
+        # find the current (original) shape of the mris and segs (expected to all be the same)
+        pulse_seq = list(sample['mris'].keys())[0]
+        current_shape = torch.tensor(sample['mris'][pulse_seq].shape)
+
+        # figure out the largest dim, then use that to find which multiple of 16 to scale the whole volume up to 
+        max_dim_size = current_shape.max()
+        multiples_of_16 = torch.tensor([16*i for i in range(1, 40)])
+        pad_up_idx = torch.where(multiples_of_16 - max_dim_size >= 0)[0][0].item()
+        pad_up_size = multiples_of_16[pad_up_idx]
+
+        # calculate the how much to pad all sides of the volume to bring it up to pad_up_size x pad_up_size x pad_up_size
+        padding1 = (pad_up_size - current_shape) // 2 + current_shape % 2
+        padding2 = (pad_up_size - current_shape) // 2
+        padding = torch.empty(6)
+        for i in range(len(padding1)):
+            padding[2*i] = padding1[i]
+            padding[2*i + 1] = padding2[i]
+        padding = tuple(padding.int().tolist())[::-1]
+
+        # pad all available volumes to yield a cube of shape pad_up_size x pad_up_size x pad_up_size
+        for k in mris.keys():
+            mris[k] = pad(mris[k], pad=padding)
+        for k in segs.keys():
+            segs[k] = pad(segs[k], pad=padding)
+        
+        assert torch.all(torch.tensor(mris[pulse_seq].shape) == pad_up_size), "An error occurred during the padding step. Debug padding?"
+
+        # resize to be of shape self.cube_size x self.cube_size x self.cube_size
+        for k in mris.keys():
+            mris[k] = interpolate(mris[k].unsqueeze(0).unsqueeze(0), size=(self.cube_size,)*3, mode='trilinear').squeeze()
+        for k in segs.keys():
+            segs[k] = interpolate(segs[k].unsqueeze(0).unsqueeze(0).float(), size=(self.cube_size,)*3, mode='nearest-exact').squeeze()
+
+        assert torch.all(torch.tensor(mris[pulse_seq].shape) == torch.tensor(self.cube_size)), "An error occurred during the interpolation step. Debug interpolation?"
+
+        return sample
+
 class CenterOnTumor(object):
     """
-    Creates a [cube_size x cube_size x cube_size] voxel centered on the tumor, 
+    Resizes all the volumes of a sample to be a [cube_size x cube_size x cube_size] voxel centered on the tumor, 
     which includes a [margin x margin x margin] around it. The pad_size is used
     to avoid index out of bounds errors for those tumors close to the edge of a volume.
     """
@@ -16,6 +71,7 @@ class CenterOnTumor(object):
         return f"{self.__class__.__name__}(cube_size={self.cube_size}, margin={self.margin}, pad_size={self.pad_size})"
     
     def __call__(self, sample):
+        # get mris and segs dicts from the input sample
         mris = sample['mris']
         segs = sample['segs']
         
